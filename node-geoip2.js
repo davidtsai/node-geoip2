0fd48a430db568076099954675d15c0e52decc06
David Tsai <david@zhurosoft.com>
2015-08-16 12:47:53 -0700

    Rename to node-geoip2.

---
+++ node-geoip2.js
@@ -0,0 +1,70 @@
+
+var mmdb = require('./lib/node_mmdb.node');
+var path = require('path');
+
+var _defaultPath = path.resolve(__dirname, './databases/GeoLite2-City.mmdb');
+var _defaultDB   = null;
+
+exports.MMDB = mmdb.MMDB;
+
+exports.init = function(path) {
+
+    _defaultDB = new mmdb.MMDB(path || _defaultPath);
+    return _defaultDB;
+}
+
+exports.lookup = function(address, callback) {
+
+    if (!_defaultDB) {
+        console.log("WARNING: ipgeo2 database not initialzed, initializing default now.");
+        exports.init();
+    }
+
+    _defaultDB.lookup(address, callback);
+}
+
+exports.lookupSync = function(address) {
+
+    if (!_defaultDB) {
+        console.log("WARNING: ipgeo2 database not initialzed, initializing default now.");
+        exports.init();
+    }
+
+    return _defaultDB.lookupSync(address);
+}
+
+function parseResult(result) {
+
+    return {
+        country:     result.country.iso_code,
+        continent:   result.continent.code,
+        postal:      result.postal ? result.postal.code : undefined,
+        city:        result.city ? result.city.names.en : undefined,
+        location:    result.location,
+        subdivision: result.subdivisions ? result.subdivisions[0].iso_code : undefined
+    };
+}
+
+exports.lookupSimple = function(address, callback) {
+
+    exports.lookup(address, function(error, result) {
+
+        if (result) {
+            callback(null, parseResult(result));
+        }
+        else {
+            callback(error, null);
+        }
+    });
+}
+
+exports.lookupSimpleSync = function(address) {
+
+    var result = exports.lookupSync(address);
+    
+    if (result) {
+        return parseResult(result);
+    }
+
+    return null;
+}
