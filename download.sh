490d2176bc13770c28039073c470a8f9b10b39c0
Torgeir Hovden <torgeir@urbanexpression.net>
2020-01-07 14:02:29 +0100

    Don't download - see https://github.com/signatu/signatu/issues/2930

--- download.sh
+++ download.sh
@@ -1,11 +1,14 @@
 #/bin/bash
 
+
+echo Not updating database - see https://dev.maxmind.com/geoip/geoip2/geolite2/
+exit 0
+
 url=https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz
 
 echo Downloading Maxmind Geo City database
 curl $url -o db.tar.gz
 tar zxvf db.tar.gz
 
-rm -rf databases
-mv GeoLite2-City* databases
+mv GeoLite2-City*/* databases
 rm db.tar.gz
